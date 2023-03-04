7d51e8ad72a5c81a45f7660c932536edf494f1cf
ethanbond64 <ethanbond39@gmail.com>
2023-03-04 10:42:15 -0700

    engine working

--- src/Engine.ts
+++ src/Engine.ts
@@ -1,30 +1,68 @@
+import Profile, { ActivityLevel } from "./Profile";
+import ProfileStorage from "./Storage/ProfileStorage";
 
 export default class Engine {
 
     private heartbeat: number;
     private sleep: number;
+    private profileStorage: ProfileStorage;
+    private report: () => void;
+    private awake: boolean;
 
     // constructor
-    constructor(heartbeat: number, sleep: number) {
+    constructor(heartbeat: number, sleep: number, report: () => void) {
         this.heartbeat = heartbeat;
         this.sleep = sleep;
+        this.report = report;
+        this.profileStorage = new ProfileStorage();
+        this.awake = true;
     }
 
     // start loop
     start() {
         // set awake
+        this.awake = true;
         // do loop
+        this.loop();
     }
 
 
     // stop loop
     stop() {
         // set asleep
+        this.awake = false;
     }
 
     // do loop
     private loop() {
+
+        console.log("looping...");
+
         // do work
+        this.report();
         // check if awake
+
         // if awake, do loop
+        this.scheduleLoop();
     }
+
+    private scheduleLoop() {
+        this.profileStorage.evaluate((profiles: Profile[]) => {
+
+            //
+            // If we have less than SLEEP logs, then we need to keep polling
+            //
+            if (this.awake && (profiles.length < this.sleep || this.sleepThreshold(profiles))) {
+                setTimeout(() => this.loop(), this.heartbeat * 1000);
+            } else {
+                this.stop();
+            }
+        });
+    }
+
+    private sleepThreshold(profiles: Profile[]): boolean {
+        return !profiles.slice(-this.sleep).every((profile: Profile) => {
+            return profile.activityLevel === ActivityLevel.Idle;
+        });
+    }
+}
--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,11 @@
+import Engine from "./Engine";
 import Profile, { ActivityLevel } from "./Profile";
 import ArrayStorage from "./Storage/ArrayStorage";
 import NumberStorage from "./Storage/NumberStorage";
 import ProfileStorage from "./Storage/ProfileStorage";
 
 const HEARTBEAT = 5;
-const SLEEP = 10;
+const SLEEP = 6;
 
 const numberStorage = new NumberStorage();
 const arrayStorage = new ArrayStorage();
@@ -27,33 +28,9 @@ const report = async () => {
   profileStorage.appendProfile(profile);
 };
 
-function reportPoll() {
+const engine = new Engine(HEARTBEAT, SLEEP, report);
 
-  report();
-
-  profileStorage.evaluate((profiles: Profile[]) => {
-
-    console.log("evaluating profile logs", profiles);
-
-    //
-    // If we have less than SLEEP logs, then we need to keep polling
-    //
-    if (profiles.length < SLEEP) {
-      setTimeout(reportPoll, HEARTBEAT * 1000);
-    }
-
-    //
-    // If the last SLEEP logs are all idle, then we can stop polling  
-    //
-    if (!profiles.slice(-SLEEP).every((profile: Profile) => {
-      return profile.activityLevel === ActivityLevel.Idle;
-    })) {
-      setTimeout(reportPoll, HEARTBEAT * 1000);
-    }
-  });
-}
-
-reportPoll();
+engine.start();
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
